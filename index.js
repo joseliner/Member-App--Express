44c69317ceec84fc74eec624ba54841206cef249
Jos Codes <joselineruiz917@gmail.com>
2022-12-03 10:30:46 -0600

    fixed css error

--- index.js
+++ index.js
@@ -10,12 +10,21 @@ const app = express();
 // app.use(logger);
 
 // Handlebars Middleware
-app.engine('handlebars', exphbs());
+//default layout is going to be main.js
+app.engine('handlebars', exphbs({ defaultLayout:'main' }))
 app.set('view engine', 'handlebars');
 
+// app.set('views', path.join(__dirname, './views'));
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname,'/public/')))
+
+// Set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 // Homepage Route
 app.get('/', (req, res) =>
@@ -25,14 +34,10 @@ app.get('/', (req, res) =>
   })
 );
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Members API Routes
-app.use('/api/members', require('./routes/api/members'));
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
 
 
